ead1c0d7d1cc2b0b2f08dce8e7074da9b9058d12
Peter Shih <ptshih@gmail.com>
2011-08-09 13:34:31 -0700

    Initial app setup

---
+++ Spotlight/SpotlightAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SpotlightAppDelegate.h
+//  Spotlight
+//
+//  Created by Peter Shih on 8/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpotlightAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end

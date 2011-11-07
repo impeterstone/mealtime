d42b36b3f5d02a7dee146f951d021baeff7de7f3
Peter Shih <ptshih@gmail.com>
2011-11-07 13:28:21 -0800

    biz data center cleanup

--- MealTime/BizDataCenter.h
+++ MealTime/BizDataCenter.h
@@ -13,18 +13,8 @@
   ASINetworkQueue *_bizQueue;
 }
 
-- (void)getPhotosFromFixturesForBiz:(NSString *)biz;
-- (void)getBizFromFixturesForBiz:(NSString *)biz;
-
 - (void)fetchBusinessForYid:(NSString *)yid;
 - (void)fetchPhotosForBiz:(NSString *)biz;
-
-- (void)fetchDetailsForPlace:(NSDictionary *)place;
-
-- (void)requestForPhotosForPlace:(NSMutableDictionary *)place;
-- (void)requestForBizForPlace:(NSMutableDictionary *)place;
-
-- (void)fetchReviewsForAlias:(NSString *)alias start:(NSInteger)start rpp:(NSInteger)rpp;
 
 - (void)cancelRequests;
 
